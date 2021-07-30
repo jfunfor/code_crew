8ea1d556106425bb2326c36f5cbb454d13d31ac6
jfun.for <e.v.kuklin1998@gmail.com>
2021-07-30 12:08:49 +0300

    function names changed

--- static/js/Footer.js
+++ static/js/Footer.js
@@ -7,13 +7,12 @@ checkboxArr.forEach(function(element) {
     element.onchange=countItemsLeft;
 })
 
-footerBtnArr[0].onclick=filterAll;
-footerBtnArr[1].onclick=filterActive;
-footerBtnArr[2].onclick=filterCompleted;
-footerBtnArr[3].onclick=filterClearCompleted;
+footerBtnArr[0].onclick=handleBtnAll;
+footerBtnArr[1].onclick=handleBtnActive;
+footerBtnArr[2].onclick=handleBtnCompleted;
+footerBtnArr[3].onclick=handleBtnClearCompleted;
 
-function countItemsLeft()
-{
+function countItemsLeft() {
     let checkboxArr = Array.from(document.getElementsByClassName('list-todos__chb'));
     let n = checkboxArr.length;
     let counterItemsLeft=0;
@@ -21,8 +20,8 @@ function countItemsLeft()
     if(!checkboxArr[i].checked)  counterItemsLeft++;
     document.getElementsByName('num_of_left')[0].value =counterItemsLeft;
 }
-function filterClearCompleted()
-{
+
+function handleBtnClearCompleted() {
     let checkboxArr = Array.from(document.getElementsByClassName('list-todos__chb'));
     let inputItemArr = Array.from(document.getElementsByClassName('inputs-style list-todos__input'));
     let n =checkboxArr.length;
@@ -35,10 +34,15 @@ function filterClearCompleted()
     document.getElementsByName('num_of_left')[0].value =n;
 }
 
-function filterActive()
-{
-    let filterBtnArr =
-        Array.from(document.getElementsByClassName('footer__but-filters'));
+function handleBtnAll() {
+    let itemArr = Array.from(document.getElementsByClassName('list-todos__item'));
+    let n =itemArr.length;
+    for (let i=0;i<n;i++)
+        itemArr[i].style.display='block';
+    handlePressedFilterBtn();
+}
+
+function handleBtnActive() {
     let checkboxArr = Array.from(document.getElementsByClassName('list-todos__chb'));
     console.log('ssss');
     let itemArr = Array.from(document.getElementsByClassName('list-todos__item'));
@@ -47,32 +51,10 @@ function filterActive()
         if(checkboxArr[i].checked)
             itemArr[i].style.display='none';
         else itemArr[i].style.display='block';
-    n=filterBtnArr.length;
-    for (let i=0;i<n;i++)
-        if(this != filterBtnArr[i])
-            filterBtnArr[i].style.border='1px solid white';
-        else filterBtnArr[i].style.border='1px solid #CCCCCC';
+    handlePressedFilterBtn();
 }
 
-function filterAll()
-{
-    let filterBtnArr =
-        Array.from(document.getElementsByClassName('footer__but-filters'));
-    let itemArr = Array.from(document.getElementsByClassName('list-todos__item'));
-    let n =itemArr.length;
-    for (let i=0;i<n;i++)
-        itemArr[i].style.display='block';
-    n=filterBtnArr.length;
-    for (let i=0;i<n;i++)
-        if(this != filterBtnArr[i])
-            filterBtnArr[i].style.border='1px solid white';
-        else filterBtnArr[i].style.border='1px solid #CCCCCC';
-}
-
-function filterCompleted()
-{
-    let filterBtnArr =
-        Array.from(document.getElementsByClassName('footer__but-filters'));
+function handleBtnCompleted() {
     let checkboxArr = Array.from(document.getElementsByClassName('list-todos__chb'));
     let itemArr = Array.from(document.getElementsByClassName('list-todos__item'));
     let n =checkboxArr.length;
@@ -80,7 +62,12 @@ function filterCompleted()
         if(!checkboxArr[i].checked)
             itemArr[i].style.display='none';
         else itemArr[i].style.display='block';
-    n=filterBtnArr.length;
+    handlePressedFilterBtn();
+}
+function handlePressedFilterBtn(){
+    let filterBtnArr =
+        Array.from(document.getElementsByClassName('footer__but-filters'));
+    let n=filterBtnArr.length;
     for (let i=0;i<n;i++)
         if(this != filterBtnArr[i])
             filterBtnArr[i].style.border='1px solid white';
